581acbfa4f2f9e7f5b6273964a099b6c16bbd55e
Lee2523 <seungoh0423@gmail.com>
2023-02-09 22:28:40 +0900

    학습 목표 추가 및 코드 변경

--- src/main/scala/MostPopularSuperheroDataset.scala
+++ src/main/scala/MostPopularSuperheroDataset.scala
@@ -7,7 +7,12 @@ import scala.io.{Codec, Source}
 
 
 /*
-broadcast를 사용하여 영화이름 표시
+목표 : 한 시리즈에서 슈퍼히어로가 다른 히어로와 같이 나오는것을 확인하여, 네트워크 모델링.
+두 히어로 간의 커넥션을 확인하는것
+
+1. 마블 그래프의 첫번째 값은 해당 히어로 ID, 나머지 값은 관련된 히어로의 수로 집계
+2. 이를 마블 네임과 조인하여 히어로 이름 찾기
+3. 가장 많은 커넥션 수를 가진 히어로 순으로 정렬.
  */
 
 
@@ -40,25 +45,22 @@ object MostPopularSuperheroDataset {
       .csv("data/Marvel-names.txt")
       .as[HeroNames]
 
-    val lines = spark.read
-      .text("data/Marvel-graph.txt")
-      .as[Hero]
+    val marvelGraph = spark.read
+      .textFile("data/Marvel-graph.txt")
 
 
-    val connections = lines
-      .withColumn("id", split(col("value"), " ")(0))
-      .withColumn("connections", size(split(col("value"), " ")) - 1)
-      .groupBy("id").agg(sum("connections").alias("connections"))
+    marvelGraph.map{
+      line =>
+        val key = line.split(" ").head.toInt
+        val value = line.split(" ").tails.length
+        (key,value)
+    }.withColumnRenamed("_1", "id")
+      .withColumnRenamed("_2", "count")
+      .groupBy("id")
+      .agg(sum("count").alias("count"))
+      .join(names, "id")
+      .sort(desc("count"))
+      .show()
 
-    val mostpopular = connections.sort($"connections".desc).first()
-
-    println(mostpopular)
-
-    val mostPopularName = names
-      .filter($"id" === mostpopular(0))
-      .select("name")
-      .first()
-
-    println(mostPopularName)
   }
 }
