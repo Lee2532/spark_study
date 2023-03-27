d945420ab9e4778cfae24bb84f786de132d2a717
Lee2523 <seungoh0423@gmail.com>
2023-03-27 22:05:28 +0900

    Streaming Code

--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -1,12 +1,46 @@
-import org.apache.log4j._
-import org.apache.spark._
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 
-/** Count up how many of each star rating exists in the MovieLens 100K data set. */
 object test {
- 
-  /** Our main function where the action happens */
-  def main(args: Array[String]) {
-   
-    println("test")
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark = SparkSession.builder()
+      .appName("StreamReaderExample")
+      .config("spark.driver.host", "localhost")
+      .config("spark.sql.streaming.checkpointLocation", "path/to/checkpoint/directory")
+      .config("spark.hadoop.fs.file.impl", "org.apache.hadoop.fs.RawLocalFileSystem")
+      .master("local[*]")
+      .getOrCreate()
+
+    val bootstrapServers = "127.0.0.1:9092"
+    val topic = "logs"
+
+    val df = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", bootstrapServers)
+      .option("subscribe", topic)
+      .option("startingOffsets", "latest")
+      .load()
+
+    val query = df
+      .writeStream
+      .option("truncate", false)
+      .option("header", true)
+      .format("csv")
+      .outputMode("append")
+      .trigger(Trigger.ProcessingTime("5 seconds"))
+      .option("path", "data/logs") // 경로를 지정해 주세요.
+      .start()
+
+    print(query)
+
+    query.awaitTermination()
   }
 }
+
+
